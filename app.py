636080d2b1b37f982418590fab12b20449beb447
Abdul Manaf <Sahitoa97@Gmail.com>
2024-05-13 11:41:33 +0500

    updates related to training

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import json
 import os
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/translate_eng_to_urdu', methods=['POST'])
 def eng_to_urdu():
