77a6085272caafd67601149902254984286ce2be
Abdul Manaf <Sahitoa97@Gmail.com>
2024-05-13 15:29:14 +0500

    updates

--- app.py
+++ app.py
@@ -12,33 +12,5 @@ def home():
 def eng_to_urdu():
     pass
 
-@app.route('/translate_eng_to_sindhi', methods=['POST'])
-def eng_to_sindhi():
-    pass
-
-@app.route('/translate_eng_to_pashto', methods=['POST'])
-def eng_to_pashto():
-    pass
-
-@app.route('/translate_eng_to_punjabi', methods=['POST'])
-def eng_to_punjabi():
-    pass
-
-@app.route('/translate_urdu_to_eng', methods=['POST'])
-def urdu_to_eng():
-    pass
-
-@app.route('/translate_sindhi_to_eng', methods=['POST'])
-def sindhi_to_eng():
-    pass
-
-@app.route('/translate_pashto_to_eng', methods=['POST'])
-def pashto_to_eng():
-    pass
-
-@app.route('/translate_punjabi_to_eng', methods=['POST'])
-def punjabi_to_eng():
-    pass
-
 if __name__ == '__main__':
     app.run(debug=True)
--- training/README.md
+++
@@ -1,4 +0,0 @@
-# Neural-Machine-Translation-NMT-
-Neural Machine Translation (NMT) using Keras. English to Urdu translation
-
-https://talharamzan-tr.medium.com/neural-machine-translation-nmt-using-keras-a71daae23e9d
