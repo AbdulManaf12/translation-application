e54ad823acc3b01c6893a2b4d47a453b60c6cd8e
Abdul Manaf <Sahitoa97@Gmail.com>
2024-05-14 11:14:39 +0500

    deployed

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from utilities import translate_sequence
 
 app = Flask(__name__)
 
@@ -6,6 +7,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/translate', methods=['POST'])
+def translate():
+    input_text = request.form['text']
+    output_text = translate_sequence(input_text)
+    return output_text
 
 if __name__ == '__main__':
     app.run(debug=True)
